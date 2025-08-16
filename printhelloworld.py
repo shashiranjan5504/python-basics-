d9953a1e258ca2ab805a9f1e679e876cdb12eb0e
shashiranjan5504 <shashiranjan@gmail.com>
2025-08-16 16:26:32 +0530

    swaptwovariables

--- printhelloworld.py
+++ printhelloworld.py
@@ -1,2 +1,5 @@
 print("hello world")
 print(2+3)
+nums=[25,12,65,34]
+print(nums)
+
---
+++ swaptwovariables.py
@@ -0,0 +1,20 @@
+a=5
+b=6
+""" method 1
+temp =a
+a=b
+b= temp
+print(a)
+print(b)
+ """
+""" method 2
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b) """
+
+# method 3 (behind the scene  it uses the concept of two rotation )
+a,b=b,a
+print(a)
+print(b)

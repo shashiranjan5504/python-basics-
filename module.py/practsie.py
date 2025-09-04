506ec4ff596f50ee9e3e55f6394c613f52b54f25
shashiranjan5504 <shashiranjan5504@gmail.com>
2025-09-04 20:13:23 +0530

    some message

--- module.py/practsie.py
+++
@@ -1,2 +0,0 @@
-print("shashi")
-print("hello")
---
+++ oops/classandobject.py
@@ -0,0 +1,9 @@
+class computer :
+
+    def config(self):
+        print("intel,32 gb, 2tb")
+
+com1=computer()
+
+computer.config(com1)
+com1.config()
